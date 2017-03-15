f13e84083f75e5f74b91caee4e218973f4a19f00
davidronaldson <davay86@gmail.com>
2017-03-15 21:46:20 +0000

    * Modularising project into different services. * Added authentication service.
    David

--- src/main/java/com/davidRonaldson/lochsideHouse/repositories/CustomerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.davidRonaldson.lochsideHouse.repositories;
-
-import com.davidRonaldson.lochsideHouse.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Long> {
-}
